d47254810592903a2fd3f900f54c92e8327fcac2
valteciraragao <118355477+valteciraragao@users.noreply.github.com>
2024-04-25 16:02:39 -0300

    primeiro desafio de apendizado basico - conta bancaria

---
+++ contaBanco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("###### Bem vindo ao DioBank, com você sempre que precisar #####");
+        System.out.println("Para abir sua conta por favor, informar os dados solicitados");
+
+
+        @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome: ");
+        String nome = scan.next();
+
+        System.out.println("Digite o seu sobrenome: ");
+        String sobrenome = scan.next();
+
+        System.out.println("Digite o sua agência: ");
+        String agencia = scan.next();
+
+        System.out.println("Digite sua conta corrente: ");
+        int numeroConta = scan.nextInt();
+
+        System.out.println("Digite o valor do seu primeiro deposito: R$ ");
+        float saldo = scan.nextFloat();
+
+        System.out.println("Olá " + nome + " " + sobrenome + " , obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + numeroConta + " e seu saldo "+ saldo + " já está disponível para saque");
+    }
+}
